1c6d7730cc42edddcbf38cb2e23ab8c012e084aa
divrawat <divrawat2001@gmail>
2024-07-02 21:45:48 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,280 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.warriorhighschool.in.net';
+
+
+export const MANGA_DESCRIPTION = ` <a href="${DOMAIN}"><u>Warrior High School: Dungeon Raid Department</u></a> is an action-packed manga series set in a world where students attend an elite academy to become powerful warriors. The story follows a group of talented and diverse students as they train, fight, and navigate intense battles and rivalries. With a mix of supernatural abilities, advanced combat techniques, and strategic warfare, the series explores themes of friendship, ambition, and the pursuit of greatness.`
+
+export const MANGA_NAME = 'Warrior High School';
+export const MANGA_AUTHOR = 'Fumiaki Maruyama';
+export const MANGA_RELEASE = '2022';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Re: Zero';
+export const MANGA_STUDIO = 'Y-Studio';
+export const MANGA_GENRE = 'Action, Adventure, Drama, Shonen, Martial Arts';
+
+export const CHAPTER_PREFIX = 'warrior-high-school-chapter';
+export const IMAGE_PREFIX = 'images/warrior-high-school';
+export const NEXT_PREVIOUS_PREFIX = 'manga/warrior-high-school-chapter';
+export const URL_PREFIX = 'manga/warrior-high-school-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/warrior-high-school-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] rounded-full w-[100px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Warrior High School';
+export const NavbarName = "Warrior High School";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. The story follows a group of talented and diverse students as they train, fight, and navigate intense battles and rivalries. With a mix of supernatural abilities, advanced combat techniques, and strategic warfare, the series explores themes of friendship, ambition, and the pursuit of greatness.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "The story is set in a world where the Earth has merged with another dimension due to a supernatural incident. This fusion has led to the emergence of dungeons filled with monsters and valuable resources. In response, special academies have been established to train individuals to become elite warriors capable of raiding these dungeons."
+    },
+    {
+        id: 2,
+        content: "The main protagonist, Yoo Jaryong, is a young man with exceptional potential. He becomes a dungeon smuggler to pay for his father’s medical bills after his father falls ill. Despite the dangers, Jaryong's skills in navigating and surviving the treacherous dungeons catch the attention of elite raiders."
+    },
+    {
+        id: 3,
+        content: "Jaryong gets the opportunity to train at Yongsa High School, one of the highest-ranked schools in South Korea, known for producing elite heroes. The school is often referred to as Warrior High School – Dungeon Raid Department. Here, Jaryong journey truly begins as he hones his abilities, learns advanced combat techniques, and forms strategic alliances with other talented students."
+    },
+    {
+        id: 4,
+        content: "The series delves into various themes such as friendship, ambition, and self-discovery. Jaryong's journey is not just about physical training but also about understanding his identity and purpose. He builds strong bonds with his peers, faces intense rivalries, and uncovers the deeper mysteries of the dungeons and the supernatural phenomena affecting the world."
+    },
+    {
+        id: 5,
+        content: "As the story progresses, Jaryong participates in numerous dungeon raids, each more challenging than the last. He encounters powerful monsters, navigates complex dungeon layouts, and competes against other teams from different academies. The narrative is filled with action-packed sequences, strategic battles, and moments of suspense."
+    },
+    {
+        id: 6,
+        content: "The manga has garnered significant attention and has been well-received by readers, amassing over 25 million views. Its unique blend of action, fantasy, and character-driven storytelling has made it a favorite among fans of the genre​ (OtakuKart)​​ (Warrior High School)​."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/black-clover.webp`,
+        title: 'Black Clover Manga',
+        link: 'https://www.blackclovermanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 13
+    }
+]
